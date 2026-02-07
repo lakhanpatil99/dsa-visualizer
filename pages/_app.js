8a030fd2dc3c29b4932b8e5123f60d5171f6b6ab
lakhanpatil99 <lakhannpatil2211@example.com>
2026-02-07 13:35:31 +0530

    Remove broken contexts to fix build

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,5 @@
 import '../styles/globals.css';
-import { AuthProvider } from "../context/AuthContext";
-import { ThemeProvider } from "../context/ThemeContext";
 
 export default function MyApp({ Component, pageProps }) {
-  return (
-    <AuthProvider>
-      <ThemeProvider>
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </AuthProvider>
-  );
+  return <Component {...pageProps} />;
 }
