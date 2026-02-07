e972c1e49cb27bba9bbbf00e4df5b6d7ec5562e9
lakhanpatil99 <lakhannpatil2211@example.com>
2026-02-07 19:04:38 +0530

    Final fix: serve ads.txt from public folder

--- pages/ads.txt.js
+++
@@ -1,2 +0,0 @@
-export default function Ads() {} 
-Ads.getInitialProps = ({ res }) => { res.setHeader('Content-Type','text/plain'); res.write('google.com, pub-860233070445192, DIRECT, f08c47fec0942fa0'); res.end(); return {}; };
--- pages/api/ads.js
+++
@@ -1,1 +0,0 @@
-export default function handler(req, res) { res.setHeader('Content-Type','text/plain'); res.status(200).send('google.com, pub-860233070445192, DIRECT, f08c47fec0942fa0'); }
