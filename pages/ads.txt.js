6bc970ed414b03cba622a399fb96c597d20d7f65
lakhanpatil99 <lakhannpatil2211@example.com>
2026-02-07 14:20:09 +0530

    Fix ads.txt for Vercel

--- pages/ads.txt.js
+++ pages/ads.txt.js
@@ -1,10 +1,2 @@
-export default function AdsTxt() {
-  return null;
-}
-
-export async function getServerSideProps({ res }) {
-  res.setHeader("Content-Type", "text/plain");
-  res.write("google.com, pub-860233070445192, DIRECT, f08c47fec0942fa0");
-  res.end();
-  return { props: {} };
-}
+export default function Ads() {} 
+Ads.getInitialProps = ({ res }) => { res.setHeader('Content-Type','text/plain'); res.write('google.com, pub-860233070445192, DIRECT, f08c47fec0942fa0'); res.end(); return {}; };
