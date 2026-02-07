3c6f9d01e6351ccf957267f2fa15276b9d284695
lakhanpatil99 <lakhannpatil2211@example.com>
2026-02-07 13:05:34 +0530

    Fix build: add AuthProvider and ThemeProvider

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,13 @@
+import '../styles/globals.css';
+import { AuthProvider } from '../context/AuthContext';
+import { ThemeProvider } from '../context/ThemeContext';
+
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <AuthProvider>
+      <ThemeProvider>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </AuthProvider>
+  );
 }
