152651843be4d7d070ddc15a5f3fdbe5296b953a
lakhanpatil99 <lakhannpatil2211@example.com>
2026-02-07 12:44:18 +0530

    Add Google AdSense auto ads script

--- pages/_document.js
+++ pages/_document.js
@@ -1,9 +1,15 @@
-import { Html, Head, Main, NextScript } from 'next/document';
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html>
+      <Head>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-860233070445192"
+          crossorigin="anonymous"
+        ></script>
+      </Head>
       <body>
         <Main />
         <NextScript />
