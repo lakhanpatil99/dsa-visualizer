714de81c7e851be3702e69ff4c6ad0b8b0a97224
lakhanpatil99 <lakhannpatil2211@example.com>
2026-02-07 12:36:44 +0530

    Fix blank page: remove broken router wrapper

--- pages/_app.js
+++ pages/_app.js
@@ -1,36 +1,5 @@
-import '../src/index.css';
-import { AuthProvider } from '../src/contexts/AuthContext';
-import { DarkModeContext } from '../src/App';
-import { useState, useEffect } from 'react';
+import '../styles/globals.css';
 
-export default function App({ Component, pageProps }) {
-  const [isDarkMode, setIsDarkMode] = useState(true);
-
-  useEffect(() => {
-    const saved = localStorage.getItem('darkMode');
-    if (saved) {
-      setIsDarkMode(JSON.parse(saved));
-    }
-  }, []);
-
-  useEffect(() => {
-    localStorage.setItem('darkMode', JSON.stringify(isDarkMode));
-    if (isDarkMode) {
-      document.documentElement.classList.add('dark');
-    } else {
-      document.documentElement.classList.remove('dark');
-    }
-  }, [isDarkMode]);
-
-  const toggleDarkMode = () => {
-    setIsDarkMode(!isDarkMode);
-  };
-
-  return (
-    <DarkModeContext.Provider value={{ isDarkMode, toggleDarkMode }}>
-      <AuthProvider>
-        <Component {...pageProps} />
-      </AuthProvider>
-    </DarkModeContext.Provider>
-  );
+export default function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
 }
